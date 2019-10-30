68e9fa1321da918d7c1755fcfb85f7f7f446beb9
Mark Ng <mark.ng.2015@sis.smu.edu.sg>
2019-10-30 21:33:29 +0800

    Added login and Retrieve components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Retrieve from './components/Retrieve';
+import SignIn from './components/SignIn';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <SignIn></SignIn>
+    );
+  }
 }
-
+ 
 export default App;
